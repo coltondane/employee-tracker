879961d9ce8d742b08ab7d6207528c28fe12df21
coltondane <113216904+coltondane@users.noreply.github.com>
2023-10-03 14:30:23 -0600

    added schema

--- Main/db/schema.sql
+++ Main/db/schema.sql
@@ -2,3 +2,28 @@ DROP DATABASE IF EXISTS employees_db;
 CREATE DATABASE employees_db;
 
 USE employees_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    dept_name VARCHAR(30) NOT NULL,
+);
+
+CREATE TABLE  role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    FOREIGN KEY (dept_id),
+    REFERENCES department(id),
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    FOREIGN KEY (role_id),
+    REFERENCES role(id),
+    ON DELETE SET NULL,
+    KEY (manager_id),
+    REFERENCES (id)
+);
