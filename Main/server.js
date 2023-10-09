b9b34a1683e52503bcf979a75e103e7d314e1c35
coltondane <113216904+coltondane@users.noreply.github.com>
2023-10-08 22:15:43 -0600

    added .env varibales to server.js

--- Main/server.js
+++ Main/server.js
@@ -13,9 +13,9 @@ app.use(express.json());
 const db = mysql.createConnection(
     {
         host: 'localhost',
-        user: 'root',
-        passqord: 'VitruvianMan',
-        databse: 'employees_db'
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        databse: process.env.DB_NAME
     },
     console.log('Connected to the employees database')
 );
